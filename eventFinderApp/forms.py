227f948e3ac9bfbda1faf02fa8baabae5d6ab76f
Ben Fitzhardinge <ben.fitzhardinge@ausvet.com.au>
2019-09-26 00:17:08 +0800

    Django ModelForm with Django Admin split field

--- eventFinderApp/forms.py
+++ eventFinderApp/forms.py
@@ -1,11 +1,18 @@
 from django.forms import ModelForm
+from django.contrib.admin import widgets
+
 from .models import Event, Category, Account
 
 
 class EventForm(ModelForm):
+
     class Meta:
         model = Event
         fields = ["host", "title", "location", "venue", "start_time", "end_time", "categories"]
+        widgets = {
+            'start_time': widgets.AdminSplitDateTime,
+            'end_time': widgets.AdminSplitDateTime,
+        }
 
 
 class AccountForm(ModelForm):
